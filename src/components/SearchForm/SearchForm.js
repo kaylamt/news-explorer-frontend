1a1d82559013f111a95a30bb5d6b032b515ac486
Kayla Thomas <kthomas2478@gmail.com>
2021-03-30 20:24:33 -0400

    header border

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -9,8 +9,8 @@ function SearchForm() {
           Find the latest news on any topic and save them in your personal account.
           </div>
         <div className="search-form__search-container">
-          <input id="avatar" type="url" className="search-form__search-field" name="avatar" placeholder="Search Here" required />
-          <button aria-label="search button" className="search-form__search-button" type="button" />
+          <input id="avatar" type="url" className="search-form__search-field" name="avatar" placeholder="Enter topic" required />
+          <button aria-label="search button" className="search-form__search-button" type="button">Search</button>
         </div>
       </div>
     </div>
