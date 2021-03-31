d31c675901bd03a3f6cf8f76dcfac733926f2889
Kayla Thomas <kthomas2478@gmail.com>
2021-03-30 20:05:47 -0400

    fixed home image

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -4,10 +4,10 @@ function SearchForm() {
       <div className="search-form__content">
         <h1 className="search-form__title">
           What's going on in the world?
-        </h1>
+          </h1>
         <div className="search-form__subtitle">
           Find the latest news on any topic and save them in your personal account.
-        </div>
+          </div>
         <div className="search-form__search-container">
           <input id="avatar" type="url" className="search-form__search-field" name="avatar" placeholder="Search Here" required />
           <button aria-label="search button" className="search-form__search-button" type="button" />
