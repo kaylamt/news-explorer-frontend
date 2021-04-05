7f6cc6de29128a0b5893dd40702fe4ba94140a31
Kayla Thomas <kthomas2478@gmail.com>
2021-04-05 17:42:46 -0400

    finished responsive layout not logged in/began navigation

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,19 +5,21 @@ import github from '../../images/github-icon.svg'
 function Footer() {
   return (
     <div className="footer">
-      <p className="footer__copyright">© 2021 Supersite, Powered by News API</p>
-      <div className="footer__links">
-        <div className="footer__page-links">
-          <p className="footer__link">Home</p>
-          <a className="footer__link" href="https://practicum.yandex.com/web/">Practicum by Yandex</a>
-        </div>
-        <div className="footer__social-links">
-          <a class="footer__social-link" href="https://github.com/kaylamt">
-            <img class="footer__social-icon" src={github} alt="github-icon"></img>
-          </a>
-          <a class="footer__social-link" href="https://www.facebook.com/kayla.thomas.9083">
-            <img class="footer__social-icon" src={facebook} alt="facebook-icon"></img>
-          </a>
+      <div className="footer__container">
+        <p className="footer__copyright">© 2021 Supersite, Powered by News API</p>
+        <div className="footer__links">
+          <div className="footer__page-links">
+            <p className="footer__link">Home</p>
+            <a className="footer__link" href="https://practicum.yandex.com/web/">Practicum by Yandex</a>
+          </div>
+          <div className="footer__social-links">
+            <a class="footer__social-link" href="https://github.com/kaylamt">
+              <img class="footer__social-icon" src={github} alt="github-icon"></img>
+            </a>
+            <a class="footer__social-link" href="https://www.facebook.com/kayla.thomas.9083">
+              <img class="footer__social-icon" src={facebook} alt="facebook-icon"></img>
+            </a>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import menu from '../../images/menu.svg';
 
 function Navigation() {
   return (
     <div className="navigation">
       <img src={menu} className="navigation__menu" alt="menu" />
+      {/* <div className="navigation__list">
+        <Link to="/" className="navigation__home-link">Home</Link>
+        <Link to="/saved-news" className="navigation__saved-articles-link">Saved articles</Link>
+        <span className="header__sign-in">Sign In</span>
+      </div> */}
     </div>
   );
 }
