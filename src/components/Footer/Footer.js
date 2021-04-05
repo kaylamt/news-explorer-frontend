94d315452b432d111bd237506c389880345b4471
Kayla Thomas <kthomas2478@gmail.com>
2021-04-05 15:39:57 -0400

    responsive footer start

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,14 +7,18 @@ function Footer() {
     <div className="footer">
       <p className="footer__copyright">© 2021 Supersite, Powered by News API</p>
       <div className="footer__links">
-        <p className="footer__link">Home</p>
-        <a className="footer__link" href="https://practicum.yandex.com/web/">Practicum by Yandex</a>
-        <a class="footer__social-link" href="https://github.com/kaylamt">
-          <img class="footer__social-icon" src={github} alt="github-icon"></img>
-        </a>
-        <a class="footer__social-link" href="https://www.facebook.com/kayla.thomas.9083">
-          <img class="footer__social-icon" src={facebook} alt="facebook-icon"></img>
-        </a>
+        <div className="footer__page-links">
+          <p className="footer__link">Home</p>
+          <a className="footer__link" href="https://practicum.yandex.com/web/">Practicum by Yandex</a>
+        </div>
+        <div className="footer__social-links">
+          <a class="footer__social-link" href="https://github.com/kaylamt">
+            <img class="footer__social-icon" src={github} alt="github-icon"></img>
+          </a>
+          <a class="footer__social-link" href="https://www.facebook.com/kayla.thomas.9083">
+            <img class="footer__social-icon" src={facebook} alt="facebook-icon"></img>
+          </a>
+        </div>
       </div>
     </div>
   );
