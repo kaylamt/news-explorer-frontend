7bb1c103effe0039b0d379eef58262204b246943
Kayla Thomas <kthomas2478@gmail.com>
2021-04-05 19:34:04 -0400

    started popup

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,10 +2,12 @@ import React from 'react';
 import SearchForm from '../SearchForm/SearchForm';
 import About from '../About/About';
 import Footer from '../Footer/Footer';
+import PopupWithForm from '../PopupWithForm/PopupWithForm';
 
 function Main() {
   return (
     <div>
+      {/* <PopupWithForm /> */}
       <SearchForm />
       <About />
       <Footer />
---
+++ src/components/PopupWithForm/PopupWithForm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function PopupWithForm(props) {
+  return (
+    <div className={`popup popup_type_${props.name} ${props.isOpen ? 'popup_opened' : ''}`} >
+      <div className="popup__container" >
+        <button aria-label="close button" className="popup__close-button" type="reset" onClick={props.onClose} />
+        <h2 className="popup__title">{props.title}</h2>
+        <form action="#" className="form form_delete" onSubmit={props.onSubmit} name={props.name}>
+          {props.children}
+          <button aria-label="save button" className="form__save-button form__save-button-delete" type="submit">{props.buttonText}</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default PopupWithForm;
