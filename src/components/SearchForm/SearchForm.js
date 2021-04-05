9599cd0dc45587197d0410bfefc379e50e5db4a2
Kayla Thomas <kthomas2478@gmail.com>
2021-04-05 16:00:09 -0400

    search from restponsive fixes

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -3,20 +3,18 @@ import Header from '../Header/Header'
 
 function SearchForm() {
   return (
-    <div className="search-form__container">
+    <div className="search-form">
       <Header />
-      <div className="search-form">
-        <div className="search-form__content">
-          <h1 className="search-form__title">
-            What's going on in the world?
+      <div className="search-form__content">
+        <h1 className="search-form__title">
+          What's going on in the world?
           </h1>
-          <div className="search-form__subtitle">
-            Find the latest news on any topic and save them in your personal account.
+        <div className="search-form__subtitle">
+          Find the latest news on any topic and save them in your personal account.
           </div>
-          <div className="search-form__search-container">
-            <input id="avatar" type="url" className="search-form__search-field" name="avatar" placeholder="Enter topic" required />
-            <button aria-label="search button" className="search-form__search-button" type="button">Search</button>
-          </div>
+        <div className="search-form__search-container">
+          <input id="avatar" type="url" className="search-form__search-field" name="avatar" placeholder="Enter topic" required />
+          <button aria-label="search button" className="search-form__search-button" type="button">Search</button>
         </div>
       </div>
     </div>
