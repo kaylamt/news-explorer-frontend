d3500be757ac25dd43a3c0e7dc88e6fa77fbc2eb
Kayla Thomas <kthomas2478@gmail.com>
2021-04-06 13:23:50 -0400

    saved-news-header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../images/header-logo.svg';
+import logoSavedNews from '../../images/logo-saved-news.svg';
+import logout from '../../images/logout.svg';
 import Navigation from '../Navigation/Navigation'
 import { CurrentUserContext } from '../../contexts/CurrentUserContext'
 
@@ -10,13 +12,18 @@ function Header(props) {
   return (
     <div className={`header header_${props.cssModifier}`}>
       <div className="header__container">
-        <img src={logo} className="header__logo" alt="logo" />
-        <div className="header__links">
+        <img src={logo} className={`header__logo header__logo_${props.cssModifier}`} alt="logo" />
+        <img src={logoSavedNews} className={`header__logo-saved-news header__logo-saved-news_${props.cssModifier}`} alt="logo" />
+        <div className={`header__links header__links_${props.cssModifier}`}>
           <Link to="/" className={`header__home-link header__home-link_${props.cssModifier}`}>Home</Link>
-          {currentUser._id &&
-            <Link to="/saved-news" className="header__saved-articles-link">Saved articles</Link>
-          }
-          <span className="header__sign-in" onClick={props.openSignInPopup}>Sign In</span>
+          {/* {currentUser._id && */}
+          <Link to="/saved-news" className={`header__saved-articles-link header__saved-articles-link_${props.cssModifier}`}>Saved articles</Link>
+          {/* } */}
+          <span className={`header__sign-in header__sign-in_${props.cssModifier}`} onClick={props.openSignInPopup}>Sign In</span>
+          <div className={`header__profile-container header__profile-container_${props.cssModifier}`}>
+            <span className={`header__profile-name header__profile-name_${props.cssModifier}`}>Elise</span>
+            <img src={logout} className={`header__profile-logout header__profile-logout_${props.cssModifier}`} alt="logout" />
+          </div>
           <Navigation />
         </div>
       </div>
