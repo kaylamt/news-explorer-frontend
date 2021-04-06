c40afbcbacc4705f39372d5e3ca7eb2d442e31df
Kayla Thomas <kthomas2478@gmail.com>
2021-04-06 14:27:02 -0400

    fixed header responsive layout

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -6,11 +6,11 @@ function Navigation() {
   return (
     <div className="navigation">
       <img src={menu} className="navigation__menu" alt="menu" />
-      {/* <div className="navigation__list">
+      <div className="navigation__list">
         <Link to="/" className="navigation__home-link">Home</Link>
         <Link to="/saved-news" className="navigation__saved-articles-link">Saved articles</Link>
         <span className="header__sign-in">Sign In</span>
-      </div> */}
+      </div>
     </div>
   );
 }
