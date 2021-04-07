3eaff60ea73424d97c949aa619331446c1aa8e0a
Kayla Thomas <kthomas2478@gmail.com>
2021-04-07 00:05:55 -0400

    header styling signed in

--- src/components/PopupWithForm/PopupWithForm.js
+++ src/components/PopupWithForm/PopupWithForm.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 function PopupWithForm(props) {
+
+
   return (
     <div className={`popup popup_type_${props.name} ${props.isOpen ? 'popup_opened' : ''}`}  >
       <div className="popup__container" >
@@ -8,7 +10,7 @@ function PopupWithForm(props) {
         <h2 className="popup__title">{props.title}</h2>
         <form action="#" className="form form_delete" onSubmit={props.onSubmit} name={props.name}>
           {props.children}
-          <button aria-label="save button" className="form__save-button form__save-button-delete" type="submit">{props.buttonText}</button>
+          <button aria-label="save button" className="form__save-button form__save-button-delete" type="submit" >{props.buttonText}</button>
           <span className="form__link" onClick={props.onFormLinkClick} >or {props.otherLink}</span>
         </form>
       </div>
