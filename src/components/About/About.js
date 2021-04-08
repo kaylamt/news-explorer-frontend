0e172267efd6ca83f27b4e8e5142548637350494
Kayla Thomas <kthomas2478@gmail.com>
2021-04-08 15:56:40 -0400

    fixed about section responsive layout

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,17 +4,19 @@ import author from '../../images/author.png';
 function About() {
   return (
     <div className="about">
-      <img src={author} className="about__author-image" alt="author" />
-      <div className="author__text-container">
-        <h1 className="author__title">
-          About the author
-        </h1>
-        <p className="author__text">
-          This block describes the project author. Here you should indicate your name, what you do, and which development technologies you know.
-        </p>
-        <p className="author__text">
-          You can also talk about your experience with Practicum, what you learned there, and how you can help potential customers.
-        </p>
+      <div className="about__container">
+        <img src={author} className="about__author-image" alt="author" />
+        <div className="author__text-container">
+          <h1 className="author__title">
+            About the author
+          </h1>
+          <p className="author__text">
+            This block describes the project author. Here you should indicate your name, what you do, and which development technologies you know.
+          </p>
+          <p className="author__text">
+            You can also talk about your experience with Practicum, what you learned there, and how you can help potential customers.
+          </p>
+        </div>
       </div>
     </div>
   );
