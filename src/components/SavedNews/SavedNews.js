533f919764c7c27ec46aaf336ad0fbcc4a09a563
Kayla Thomas <kthomas2478@gmail.com>
2021-04-08 14:25:29 -0400

    sign-in popup and card fixes

--- src/components/SavedNews/SavedNews.js
+++ src/components/SavedNews/SavedNews.js
@@ -12,7 +12,7 @@ function SavedNews() {
       date: "November 4, 2020",
       source: "treehugger",
       link: "https://www.treehugger.com/special-sit-spot-nature-5085811",
-      image: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.dailycompass.org%2Fwp-content%2Fuploads%2F2012%2F07%2Fwoods.jpeg&f=1&nofb=1",
+      image: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fthewowstyle.com%2Fwp-content%2Fuploads%2F2015%2F01%2Fnature-images..jpg&f=1&nofb=1",
       _id: 1,
     },
     {
