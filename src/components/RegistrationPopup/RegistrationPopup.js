4e0cb76293b1e0d30acdd60bdc4238fe8b58c599
Kayla Thomas <kthomas2478@gmail.com>
2021-04-09 11:41:44 -0400

    registration popup

--- src/components/RegistrationPopup/RegistrationPopup.js
+++ src/components/RegistrationPopup/RegistrationPopup.js
@@ -5,8 +5,8 @@ function RegistrationPopup(props) {
     <div className={`popup popup_type_registration ${props.isOpen ? 'popup_opened' : ''}`}  >
       <div className="popup__container" >
         <button aria-label="close button" className="popup__close-button" type="reset" onClick={props.onClose} />
-        <h2 className="popup__title">{props.title}</h2>
-        <p className="form__link" onClick={props.onFormLinkClick} >Sign In</p>
+        <h2 className="popup__registration-title">{props.title}</h2>
+        <p className="form__registration-link" onClick={props.onFormLinkClick} >Sign In</p>
       </div>
     </div>
   );
