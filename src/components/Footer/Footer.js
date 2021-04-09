7fae7c62de7c6fb589632c9984de76af05bb9c48
Kayla Thomas <kthomas2478@gmail.com>
2021-04-09 14:42:27 -0400

    footer ul

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,16 +10,24 @@ function Footer() {
         <p className="footer__copyright">© {new Date().getFullYear()} Supersite, Powered by News API</p>
         <div className="footer__links">
           <ul className="footer__page-links">
-            <Link to="/" className="footer__link">Home</Link>
-            <a className="footer__link" href="https://practicum.yandex.com/web/" target="_blank" rel="noreferrer">Practicum by Yandex</a>
+            <li className="footer__link">
+              <Link to="/" className="footer__link-home">Home</Link>
+            </li>
+            <li className="footer__link">
+              <a className="footer__link-practicum" href="https://practicum.yandex.com/web/" target="_blank" rel="noreferrer">Practicum by Yandex</a>
+            </li>
           </ul>
           <ul className="footer__social-links">
-            <a className="footer__social-link" href="https://github.com/kaylamt" target="_blank" rel="noreferrer">
-              <img className="footer__social-icon" src={github} alt="github-icon"></img>
-            </a>
-            <a className="footer__social-link" href="https://www.facebook.com/kayla.thomas.9083" target="_blank" rel="noreferrer" >
-              <img className="footer__social-icon" src={facebook} alt="facebook-icon"></img>
-            </a>
+            <li className="footer__social-link">
+              <a className="footer__social-link-github" href="https://github.com/kaylamt" target="_blank" rel="noreferrer">
+                <img className="footer__social-icon" src={github} alt="github-icon"></img>
+              </a>
+            </li>
+            <li className="footer__social-link">
+              <a className="footer__social-link-facebook" href="https://www.facebook.com/kayla.thomas.9083" target="_blank" rel="noreferrer" >
+                <img className="footer__social-icon" src={facebook} alt="facebook-icon"></img>
+              </a>
+            </li>
           </ul>
         </div>
       </div>
