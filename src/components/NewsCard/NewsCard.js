604d582d685a18710d85f4c0ee135ca2b4794c95
Kayla Thomas <kthomas2478@gmail.com>
2021-04-09 11:48:26 -0400

    new-card-list

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -36,13 +36,13 @@ function NewsCard(props) {
   }
 
   return (
-    <div className="news-card">
-      <ul className="news-card__image-info">
+    <li className="news-card">
+      <div className="news-card__image-info">
         <p className="news-card__keyword">{props.article.keyword}</p>
         {actionButton()}
         <img className="news-card__image" alt={props.article.title} src={props.article.image} />
-      </ul>
-      <ul className="news-card__info">
+      </div>
+      <div className="news-card__info">
         <div className="news-card__title-container">
           <p className="news-card__date">{props.article.date}</p>
           <h2 className="news-card__title">{props.article.title}</h2>
@@ -50,8 +50,8 @@ function NewsCard(props) {
         <p className="news-card__text">{props.article.text}</p>
         <a className="news-card__link" href={props.article.link}>{props.article.source}
         </a>
-      </ul>
-    </div>
+      </div>
+    </li>
   );
 };
 
--- src/components/NewsCardList/NewsCardList.js
+++ src/components/NewsCardList/NewsCardList.js
@@ -3,13 +3,13 @@ import NewsCard from '../NewsCard/NewsCard';
 
 function NewsCardList(props) {
   return (
-    <section className="news-card-list">
+    <ul className="news-card-list">
       <div className="news-card-list__container">
         {props.articles.map((article) => (
           <NewsCard article={article} key={article._id} inSearchResults={props.inSearchResults} />
         ))}
       </div>
-    </section >
+    </ul >
   );
 };
 
